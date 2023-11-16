4a72067849bcb9435531a59f116c5d547718c032
Tega Rorobi <rorobitega@gmail.com>
2023-11-16 22:21:18 +0100

    Started a new app, 'api' and registered it in the settings. Also created a UserSerializer.

---
+++ api/serializers.py
@@ -0,0 +1,10 @@
+
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = '__all__'
