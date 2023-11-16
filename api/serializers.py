ca0a86cd45a8150759c1d094846e1432d8ffd9fa
Tega Rorobi <rorobitega@gmail.com>
2023-11-16 22:48:51 +0100

    Created a UsersViewSet for performing CRUD operations on the User Model.

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,7 @@
 
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
 
+from django.contrib.auth import get_user_model
 UserModel = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from .serializers import *
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+UserModel = get_user_model()
+
+
+class UsersViewSet(viewsets.ModelViewSet):
+
+    "API Viewset to list out, create, retrieve, update and delete users."
+
+    queryset = UserModel.objects.order_by('-id')
+    serializer_class = UserSerializer
