01b1528e02e683547662e32700842caebd062efd
Tega Rorobi <rorobitega@gmail.com>
2023-11-17 11:03:59 +0100

    Created a BaseModel class

--- base/models.py
+++ base/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+
+    "Base Model class possessing useful timestamps and a utility method and function."
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def new(cls, **kwargs) -> models.Model:
+        "Create and save a new model instance"
+        ins = cls.objects.create(**kwargs)
+        return ins
+
+    def update(self, **kwargs) -> None:
+        for k, v in kwargs.items():
+            if hasattr(self, k):
+                setattr(self, k, v)
+        self.save()
