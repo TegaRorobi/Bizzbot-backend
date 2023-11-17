395e6a442a5460eaca5cbae48a8da434ef05d006
Tega Rorobi <rorobitega@gmail.com>
2023-11-17 09:02:27 +0100

    Added swagger documentation. (to the root urlconf for now, though it highlights most endpoints)

--- core/settings.py
+++ core/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'rest_framework_simplejwt.token_blacklist',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -1,23 +1,34 @@
 """
 URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+
+from rest_framework.permissions import AllowAny
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Bizzbot API',
+        default_version='v1',
+        description='API Documentation for the Bizzbot Application',
+        contact=openapi.Contact(email="support@bizzbot.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(AllowAny,)
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+
+    # swagger/redoc
+    re_path('^api/swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(), name='schema-json'),
+    re_path('^api/swagger/?$', schema_view.with_ui('swagger'), name='schema-swagger'),
+    re_path('^api/redoc/?$', schema_view.with_ui('redoc'), name='schema-redoc'),
 ]
