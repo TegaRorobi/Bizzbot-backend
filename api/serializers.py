4007119344cd1f8f3b4f73407a381b1c61a20d3c
Tega Rorobi <rorobitega@gmail.com>
2023-11-17 12:32:52 +0100

    Fixed the bug with the UserSerializer by adding the CountryFieldMixin

--- api/serializers.py
+++ api/serializers.py
@@ -2,10 +2,11 @@
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
 
+from django_countries.serializers import CountryFieldMixin
 from django.contrib.auth import get_user_model
 UserModel = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(CountryFieldMixin, serializers.ModelSerializer):
     class Meta:
         model = UserModel
         fields = '__all__'
