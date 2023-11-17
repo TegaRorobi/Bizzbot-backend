8daf5c03001b72152c229a4be1cb18a1d9e581a1
Tega Rorobi <rorobitega@gmail.com>
2023-11-17 08:17:21 +0100

    Removed the unique constraint from the username field.

--- user/models.py
+++ user/models.py
@@ -37,7 +37,6 @@ class User(AbstractUser):
     username = models.CharField(
         _("username"),
         max_length=150,
-        unique=True,
         help_text=_("Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."),
         validators=[UnicodeUsernameValidator()],
         error_messages={"unique": _("A user with that username already exists."),},
