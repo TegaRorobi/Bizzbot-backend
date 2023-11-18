a760747057436d38304115b0bad3dd5b6a6fc289
Tega Rorobi <rorobitega@gmail.com>
2023-11-18 04:38:16 +0100

    Registered the Product model in the admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    model = Product 
+    list_display = 'seller', 'price', 'inventory', 'delivery_time'
+
+    @admin.display()
+    def seller(self, obj):
+        return obj.owner.full_name
