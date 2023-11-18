70c6ce06e5152c50e9ef273e7ea338cf9f6c7f31
Tega Rorobi <rorobitega@gmail.com>
2023-11-18 05:31:49 +0100

    Made changes to the Product model.

--- api/admin.py
+++ api/admin.py
@@ -4,8 +4,8 @@ from .models import *
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     model = Product 
-    list_display = 'seller', 'price', 'inventory', 'delivery_time'
+    list_display = 'seller', 'name', 'price', 'inventory', 'delivery_time'
 
     @admin.display()
     def seller(self, obj):
-        return obj.owner.full_name
+        return obj.seller.full_name
--- api/models.py
+++ api/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 from base.models import BaseModel
 from django.utils.translation import gettext as _
+from datetime import timedelta
 
 from django.contrib.auth import get_user_model
 UserModel = get_user_model()
 
 
 class Product(BaseModel):
-    owner = models.ForeignKey(UserModel, verbose_name=('Product Owner'), on_delete=models.CASCADE)
+    seller = models.ForeignKey(UserModel, verbose_name=('Product Owner'), on_delete=models.CASCADE)
     name = models.CharField(_('Product Name'), max_length=255)
     price = models.DecimalField(_('Product Price'), max_digits=15, decimal_places=2, default=0.00)
     inventory = models.IntegerField(_('Product Inventory'), default=0)
-    delivery_time = models.TimeField(null=True, blank=True)
+    delivery_time = models.DurationField(default=timedelta(hours=1, minutes=30))
 
     def __str__(self) -> str:
-        return f"{self.name} {self.price}"
+        return f"{self.name} [{self.price}]"
